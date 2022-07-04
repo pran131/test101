f5596dd455afa7de36182b4a4fbcd08fd66c18ca
pran131 <pran131@gmail.com>
2022-07-05 01:54:46 +0530

    cloudtrailcommit

--- main.tf
+++ main.tf
@@ -6,3 +6,10 @@ resource "aws_vpc" "myVPC" {
     Name = "Test_Env"
   }
 }
+
+resource "aws_cloudtrail" "cloudtrail101" {
+  name                          = var.cloudtrailname
+  s3_bucket_name                = "tfstatebycket"
+  s3_key_prefix                 = "logs"
+  include_global_service_events = false
+}
--- variables.tfvars
+++ variables.tfvars
@@ -1,1 +1,2 @@
 cidr = "10.0.0.0/16"
+cloudtrailname = "logbench101"
